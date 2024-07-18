e6597af59964431bfe9bd333462dc6203dbffb3a
Paco <pacodeoliveirasaavedra@gmail.com>
2024-07-18 00:12:32 -0300

    feat : subir pro grafico , ja esta extraindo a cada 60 sec e jogando no CSV (manual)

--- src/extract_load.py
+++ src/extract_load.py
@@ -15,14 +15,33 @@ print("Chave da API carregada:", AV_API_KEY)
 ts = TimeSeries(key=AV_API_KEY, output_format='pandas')
 
 # Realizando a busca pelo símbolo 'itau'
-data, meta_data = ts.get_symbol_search('itau')
+data, meta_data = ts.get_symbol_search('apple')
+
+dataitau, meta_dataitau = ts.get_symbol_search('itau')
+
+
 
 # Salvando em arquivo csv
-#data.to_csv('dados_itau.csv')
+#data.to_csv('dados_extract.csv')
 
-
+"""
 print("Dados:")
 print(data)
 
 print("\nMetadados:")
 print(meta_data)
+
+print("\nDados:")
+print(dataitau)
+
+print("\nMetadados:")
+print(meta_dataitau)
+
+"""
+data3,meta_data3 = ts.get_intraday(symbol= 'AAPL',interval= '60min', outputsize= 'full')
+data_filtered1hr = data3.loc['2024-07-17']
+print("\n Dados Daily AAPL ")
+print(data_filtered1hr)
+
+
+data_filtered1hr.to_csv('dados_filtered.csv')
