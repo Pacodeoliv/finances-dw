8c1bee6cccd8528dc995d71a5aa850f4976f1ca4
Paco <pacodeoliveirasaavedra@gmail.com>
2024-08-30 04:17:02 -0300

    atualizado pra data de hoje , inserindo grafico tentativa 1

--- src/extract_load.py
+++ src/extract_load.py
@@ -4,6 +4,8 @@ import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 from dotenv import load_dotenv
 import os 
+import plotly.graph_objects as go
+import plotly.io as pio
 load_dotenv()
 
 AV_API_KEY = os.getenv('API_KEY')
@@ -39,9 +41,57 @@ print(meta_dataitau)
 
 """
 data3,meta_data3 = ts.get_intraday(symbol= 'AAPL',interval= '60min', outputsize= 'full')
-data_filtered1hr = data3.loc['2024-07-17']
+data_filtered1hr = data3.loc['2024-08-29']
 print("\n Dados Daily AAPL ")
 print(data_filtered1hr)
 
 
-data_filtered1hr.to_csv('dados_filtered.csv')
+data_filtered1hr.to_csv('dados_filtered29.csv')
+
+
+# Configure o renderizador (notebook ou browser, conforme necessário)
+pio.renderers.default = 'browser'
+
+# Seus dados em um DataFrame
+data = {
+    'date': ['2024-08-29 19:00:00', '2024-08-29 18:00:00', '2024-08-29 17:00:00', '2024-08-29 16:00:00'],
+    'open': [230.05, 230.26, 230.21, 229.83],
+    'close': [230.29, 230.00, 230.31, 230.259]
+}
+
+df = pd.DataFrame(data)
+
+# Convertendo o campo 'date' para datetime
+df['date'] = pd.to_datetime(df['date'])
+
+# Criando o gráfico
+fig = go.Figure()
+
+# Adicionando a série 'open'
+fig.add_trace(go.Bar(
+    x=df['open'],
+    y=df['date'],
+    orientation='h',
+    name='Open',
+    marker=dict(color='royalblue')
+))
+
+# Adicionando a série 'close'
+fig.add_trace(go.Bar(
+    x=df['close'],
+    y=df['date'],
+    orientation='h',
+    name='Close',
+    marker=dict(color='orange')
+))
+
+# Atualizando o layout
+fig.update_layout(
+    title='Abertura e Fechamento da AAPL ao Longo do Tempo',
+    xaxis_title='Valor (USD)',
+    yaxis_title='Horário',
+    barmode='group'
+)
+
+# Exibindo o gráfico
+fig.show()
