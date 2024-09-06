0cff28b299a723444d753cd4e892ff2d61ce0275
Paco <pacodeoliveirasaavedra@gmail.com>
2024-09-06 11:11:15 -0300

    Extract (coleta de dados) validando API #OK

--- src/extractv1.py
+++ src/extractv1.py
@@ -2,16 +2,15 @@ import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 from dotenv import load_dotenv
 import os
-load_dotenv
+load_dotenv()
 
 
 # Projeto 1, coletar dados da api que me trás coisas da bolsa de valores e salvar em csv. (Quero aprender como fazer request em API e como escrever arquivos)
 
 alphaapikey = os.getenv('API_KEY')
 
-if alphaapikey is True:
+if alphaapikey :
     print("Chave da API carregada")
-
-if alphaapikey is None:
+else:
     raise ValueError("A chave da API não foi encontrada. Verifique se o arquivo está configurado corretamente")
 
